2a479bc10f740c4760e5444bfbf40c02885fa38f
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-09-18 22:09:56 +0600

    add filter box

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -3,12 +3,58 @@ import useProducts from '../../Hook/useProducts';
 import ProductDetails from './ProductDetails';
 
 const Dashboard = () => {
-const [products, setProducts] = useProducts();
-console.log(products);
+    const [products, setProducts] = useProducts();
+
+    console.log(products);
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mx-auto w-full'>
-            { products?.map(product => <ProductDetails key={product?._id} product={product} setProducts={setProducts} />)
-            }
+        <div className="">
+            <div className="head bg-violet-800 py-3 items-center px-10 flex flex-col lg:flex-row gap-4 gap">
+                <div className="searchbar grow ">
+                    <div className="form-control data-svelte-search">
+                        <input
+                            type="text"
+                            placeholder="Type to search..."
+                            className="input px-7 py-2 placeholder-slate-300 text-white bg-violet-700 hover:border-none border-none focus:ring-0 focus:ring-offset-0 text-md"
+                        />
+                    </div>
+                </div>
+                <div className="filteringBy-A-to-Z">
+                    <select className="select w-full max-w-xs input px-3 placeholder-white bg-violet-700 hover:border-none border-none focus:ring-0 focus:ring-offset-0 text-white text-sm">
+                        <option disabled selected>
+                            Sort by: Price
+                        </option>
+                        <option>Name</option>
+                        <option>Date</option>
+                        <option>Type</option>
+                        <option>Lists</option>
+                        <option>Size</option>
+                    </select>
+                </div>
+                <div className="allStatus">
+                    <select className="select w-full max-w-xs px-3 placeholder-white bg-violet-700 hover:border-none border-none focus:ring-0 focus:ring-offset-0 text-white text-md">
+                        <option disabled selected>
+                            All Category
+                        </option>
+                        <option>Recently</option>
+                        <option>Last day</option>
+                        <option>Last week</option>
+                        <option>Last month</option>
+                        <option>Last year</option>
+                    </select>
+                </div>
+                <div className="recipients">
+                    <button className="input px-3 text-white placeholder-white bg-violet-700 hover:border-none border-none focus:ring-0 focus:ring-offset-0 text-white-50 text-md">
+                        <span className="font-bold text-md">
+                            {/* <FontAwesomeIcon className="mr-4" icon={faPlus} /> */}
+                        </span>
+                        New Recipients
+                    </button>
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-5 mx-auto w-full'>
+                {products?.map(product => <ProductDetails key={product?._id} product={product} />)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Dashboard/ProductDetails.js
+++ src/Components/Dashboard/ProductDetails.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const ProductDetails = ({ product }) => {
-    const { _id, category, price, img, name, ratings, seller, shipping } = product;
+    const {category, price, img, name} = product;
     return (
         <div className="card w-5/6 group bg-base-100 shadow-xl hover:shadow-2xl hover:shadow-gray-500 rounded-xl mx-auto">
-            <figure><img className='w-4/5 mx-auto rounded-full p-3 m-5 group-hover:scale-105 group-hover:rotate-45 hover:border border-red-700 duration-200' src={img} alt="Shoes" /></figure>
+            <figure><img className='w-4/5 mx-auto rounded-full group-hover:shadow p-3 m-5 group-hover:scale-105 group-hover:rotate-45 hover:border border-red-700 duration-200' src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title font-bold text-start ml-5">Name: {name}</h2>
                 <div className="flex flex-col justify-start w-full text-start mx-5 py-2">
