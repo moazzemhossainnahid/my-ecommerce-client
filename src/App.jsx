8c14dbc5864e57a8e5c7bd03d1e9a8a407ed9a91
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-09-19 11:19:39 +0600

    Auth Setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Home from './Components/Home/Home';
+import SignIn from './Components/Login/SignIn';
+import SignUp from './Components/Login/SingUp';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<SignIn/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/signup' element={<SignUp/>} />
         <Route path='/dashboard' element={<Dashboard/>} />
       </Routes>
     </div>
